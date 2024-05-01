117b1fc324e892cfda42cab3d36ce27624b84bbd
umerBlueiris <umer@blueirissoft.com>
2024-05-01 14:53:42 +0400

    new

--- connection.js
+++ connection.js
@@ -8,9 +8,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(require("./routes/route"));
 
-mongoose.connect(process.env.DATABASE).then(() => {
-  console.log("DB Connected");
-});
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: "smsdatas", // Add your database name here
+//   })
+//   .then(() => {
+//     console.log("DB Connected");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to database:", err);
+//   });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(
